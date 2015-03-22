f72aa2eb200455a053d8296748b5d0bc3a2f0b6f
Josef Ottosson <ottossonjosef@gmail.com>
2015-03-22 14:09:26 +0100

    Added JsonProperty

--- ContentJson/Models/SelectOne/SelectOptionDto.cs
+++ ContentJson/Models/SelectOne/SelectOptionDto.cs
@@ -1,9 +1,16 @@
-﻿namespace ContentJson.Models.SelectOne
+﻿using Newtonsoft.Json;
+
+namespace ContentJson.Models.SelectOne
 {
     public class SelectOptionDto
     {
+        [JsonProperty("value")]
         public string Value { get; set; }
+
+        [JsonProperty("text")]
         public string Text { get; set; }
+
+        [JsonProperty("selected")]
         public bool Selected { get; set; }
     }
 }
