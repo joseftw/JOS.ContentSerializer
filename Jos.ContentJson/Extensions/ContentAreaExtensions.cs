e6573acc205286eaa213bfe874f55ae86346dc97
Josef Ottosson <ottossonjosef@gmail.com>
2015-04-16 21:32:04 +0200

    Readded wrapItems to contentarea...damn merge.....

--- Jos.ContentJson/Extensions/ContentAreaExtensions.cs
+++ Jos.ContentJson/Extensions/ContentAreaExtensions.cs
@@ -3,16 +3,49 @@ using System.Linq;
 using EPiServer;
 using EPiServer.Core;
 using EPiServer.ServiceLocation;
+using Jos.ContentJson.Helpers;
 using Newtonsoft.Json;
 
 namespace Jos.ContentJson.Extensions
 {
     public static class ContentAreaExtensions
     {
-        public static Dictionary<string, object> GetStructuredDictionary(this ContentArea contentArea)
+        public static string ToJson(this ContentArea contentArea, bool wrapItems = true)
         {
-            var groupedContentTypes = contentArea.Items.GroupBy(x => x.GetContent().ContentTypeID);
+            var structuredData = GetStructuredDictionary(contentArea, wrapItems);
+            var json = JsonConvert.SerializeObject(structuredData);
+            return json;
+        }
+
+        /// <summary>
+        /// Returns the data in the contentArea either as a Dictionary or a List.
+        /// </summary>
+        /// <param name="contentArea"></param>
+        /// <param name="wrapItems">
+        /// If set to true, the items will be wrapped in it's own "namespace", useful when working with different
+        /// contenttypes in the contentarea
+        /// If false, it will be returned as an list of objects.
+        /// </param>
+        /// <returns></returns>
+        public static object GetStructuredDictionary(this ContentArea contentArea, bool wrapItems = true)
+        {
+            var contentJsonHelper = new ContentJsonHelper();
+
+            if (!wrapItems)
+            {
+                var contentAreaItems = contentArea.Items;
+                var propertyList = new List<object>();
+                foreach (var contentAreaItem in contentAreaItems)
+                {
+                    var loadedContentAreaItem = contentJsonHelper.GetLoadedContentAreaItem(contentAreaItem);
+                    propertyList.Add(loadedContentAreaItem);
+                }
+
+                return propertyList;
+            }
+
             var propertyDict = new Dictionary<string, object>();
+            var groupedContentTypes = contentArea.Items.GroupBy(x => x.GetContent().ContentTypeID);
 
             foreach (var contentType in groupedContentTypes)
             {
@@ -24,13 +57,7 @@ namespace Jos.ContentJson.Extensions
 
             return propertyDict;
         }
-
-        public static string ToJson(this ContentArea contentArea)
-        {
-            var propertiesDict = GetStructuredDictionary(contentArea);
-            return JsonConvert.SerializeObject(propertiesDict);
-        }
-
+    
         private static List<object> GetContentTypeAsList(IEnumerable<ContentAreaItem> contentType)
         {
             var contentLoader = ServiceLocator.Current.GetInstance<IContentLoader>();
