4f5a8f37e788c1e6be7d7c7e6888dca9dbcd9a97
Josef Ottosson <josef.ottosson@creuna.se>
2016-08-24 10:42:33 +0200

    Restored XhtmlString support.

---
+++ Jos.ContentJson/Interfaces/IXhtmlStringPropertyHelper.cs
@@ -0,0 +1,12 @@
+﻿using EPiServer.Core;
+
+using Jos.ContentJson.Models.Dtos;
+
+namespace Jos.ContentJson.Interfaces
+{
+	public interface IXhtmlStringPropertyHelper
+	{
+		XhtmlString CastProperty(object propertyValue);
+		string GetStructuredData(StructuredDataDto dataDto);
+	}
+}
