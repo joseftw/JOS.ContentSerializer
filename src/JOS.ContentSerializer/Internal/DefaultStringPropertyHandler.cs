c3ce2c8c8a547e8c3447153dcdc8a5d8b1dd09ce
Josef Ottosson <ottossonjosef@gmail.com>
2017-05-21 16:22:59 +0200

    Fixes references and nuspec

--- src/JOS.ContentSerializer/Internal/DefaultStringPropertyHandler.cs
+++ src/JOS.ContentSerializer/Internal/DefaultStringPropertyHandler.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using System.Reflection;
 using EPiServer.Core;
-using EPiServer.Shell.ObjectEditing;
 using System.Linq;
+using EPiServer.Shell.ObjectEditing;
 
 namespace JOS.ContentSerializer.Internal
 {
